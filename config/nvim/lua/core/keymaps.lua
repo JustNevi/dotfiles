130112b2c88f250eb49fc58393d4e0287826a1ed
JustNevi <122728617+JustNevi@users.noreply.github.com>
2025-05-27 09:40:26 +0200

    style: nvim lualine

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -1,3 +1,5 @@
 local opt = { noremap = true, silent = true }
 
-vim.keymap.set("v", "Y", '"+y', opt) 
+vim.keymap.set("v", "Y", '"+y', opt)
+vim.api.nvim_set_keymap('v', '<Tab>', '>gv', { noremap = true, silent = true, desc = 'Indent visual selection' })
+vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true, silent = true, desc = 'Dedent visual selection' })
