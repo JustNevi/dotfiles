bfc3fd843fdf57189e30290fcab9f415cffcca49
JustNevi <122728617+JustNevi@users.noreply.github.com>
2025-07-19 20:00:24 +0200

    chore: xresources patches for dwm

--- config/suckless/blocks/blocks.def.h
+++ config/suckless/blocks/blocks.def.h
@@ -3,7 +3,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", BIN_PATH "sb-memory",					5,		4},
+	{"", BIN_PATH "sb-memory",					5,		5},
+	{"", BIN_PATH "sb-internet",				5,		4},
 	{"", BIN_PATH "sb-volume",					5,		3},
 	{"", BIN_PATH "sb-battery",				5,	   2},
 	{"", BIN_PATH "sb-date",					5,		1},
