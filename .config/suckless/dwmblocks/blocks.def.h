5258fa84661be89d95b25ff964662418c0b82b8d
JustNevi <122728617+JustNevi@users.noreply.github.com>
2026-08-21 12:54:44 +0300

    feat: base dwmblocks

--- .config/suckless/dwmblocks/blocks.def.h
+++ .config/suckless/dwmblocks/blocks.def.h
@@ -1,15 +1,11 @@
-#define BIN_PATH "~/.local/bin/statusbar/"
-
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", BIN_PATH "sb-memory",					5,		5},
-	{"", BIN_PATH "sb-internet",				5,		4},
-	{"", BIN_PATH "sb-volume",					5,		3},
-	{"", BIN_PATH "sb-battery",				5,	   2},
-	{"", BIN_PATH "sb-date",					5,		1},
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
